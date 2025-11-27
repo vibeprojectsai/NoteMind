2101434b3d9a6df8aba7545c96c950eac31275a6
mayankraval13 <iammayankraval@gmail.com>
2025-11-27 11:46:27 +0530

    fix: remove node_modules, add .gitignore, fix vercel build command

--- test-pdf-import.ts
+++
@@ -1,8 +0,0 @@
-import * as pdfParse from "pdf-parse";
-console.log("pdfParse exports:", pdfParse);
-try {
-  const { PDFParse } = require("pdf-parse");
-  console.log("PDFParse named export:", PDFParse);
-} catch (e) {
-  console.log("Error requiring pdf-parse:", e);
-}
